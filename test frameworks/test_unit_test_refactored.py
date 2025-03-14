d6962c683088476ca9d6f45e5f8a9865236f9f91
Zotov.I <ivan151698@gmail.com>
2025-03-14 17:16:07 +0300

    add pytest

--- test frameworks/test_unit_test_refactored.py
+++ test frameworks/test_unit_test_refactored.py
@@ -8,11 +8,12 @@ class TestUniqueSelectors(unittest.TestCase):
 
     def fill_form(self, link):
         browser = self.driver
+        browser.implicitly_wait(5)
         browser.get(link)
 
-        browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('Ivan')
-        browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Dorogov')
-        browser.find_element(By.CSS_SELECTOR, '.third_class .third').send_keys('sawed@gmail.com')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.first').send_keys('Ivan')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.second').send_keys('Dorogov')
+        browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.third').send_keys('sawed@gmail.com')
         browser.find_element(By.CSS_SELECTOR, "button.btn").click()
 
         welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
