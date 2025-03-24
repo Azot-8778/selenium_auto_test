19adcdee88c93d43ad00408af15e5f61b0e4f56e
Zotov.I <ivan151698@gmail.com>
2025-03-24 17:48:30 +0300

    stepik login done

--- test frameworks/test_login.py
+++ test frameworks/test_login.py
@@ -4,9 +4,10 @@ import time
 import json
 import pytest
 import math
-
-
-answer = math.log(int(time.time()))
+import unittest
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture(scope="session")
 def load_config():
@@ -19,32 +20,46 @@ def load_config():
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    options = Options()
+    options.page_load_strategy = 'eager'
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
 
 class TestLogin:
     welcome_text = ""
-    numbers = ["95", "96","97","98","99","03","04","05"]
+    numbers = ["895", "896","897","898","899","903","904","905"]
 
     @pytest.mark.parametrize('links', numbers)
     def test_login_link(self, browser, links, load_config):
 
-        link = f"https://stepik.org/lesson/2368{links}/step/1"
+        link = f"https://stepik.org/lesson/236{links}/step/1"
         browser.get(link)
         browser.implicitly_wait(10)
         login = load_config['login_stepik']
         password = load_config['password_stepik']
 
-        enter_button = browser.find_element(By.ID, "ember466").click()
-        time.sleep(10)
+        browser.find_element(By.CSS_SELECTOR, ".ember-view.navbar__auth.navbar__auth_login.st-link.st-link_style_button").click()
         field_login = browser.find_element(By.NAME, "login")
         field_login.send_keys(login)
         field_login = browser.find_element(By.NAME, "password")
         field_login.send_keys(password)
         button = browser.find_element(By.XPATH, "//button[@type='submit']")
         button.click()
+        time.sleep(5)
+
+        input_graph = browser.find_element(By.TAG_NAME, "textarea")
+        answer = str(math.log(int(time.time())))
+        input_graph.send_keys(answer)
+
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".submit-submission"))).click()
+        message = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint'))).text
+        assert message == 'Correct!', f"Should be correct, not {message}"
+
+if __name__ == "__main__":
+    unittest.main()
+
 
 
 
